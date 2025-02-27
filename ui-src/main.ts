877d464cf922a8e5f1b14f694497a86884082675
Kazuhiro Yamato <kz0817@gmail.com>
2025-02-28 08:06:30 +0900

    Show file list.

--- ui-src/main.ts
+++ ui-src/main.ts
@@ -1,3 +1,38 @@
 document.addEventListener('DOMContentLoaded', () => {
-    console.log('Hello');
+    showFileList('/');
 });
+
+interface DirEntry {
+  name: string;
+  isDir: boolean;
+  size: number;
+  modifiedTime: string;
+}
+
+interface FileList {
+  location: string;
+  entries: DirEntry[];
+}
+
+const showFileList = async (dir: string): Promise<void> => {
+  const url = `/api/filelist?location=${dir}`;
+  const response = await fetch(url);
+  if (!response.ok) {
+    throw new Error(`Failed to call {url}`)
+  }
+  console.log(response);
+  const fileList: FileList = await response.json();
+  console.log(fileList);
+
+  document.getElementById('location')!.textContent = fileList.location;
+
+  const entriesElem: HTMLUListElement = document.getElementById('entries') as HTMLUListElement;
+
+  fileList.entries
+  .map((entry: DirEntry) => {
+    const liElem = document.createElement('li');
+    liElem.textContent = entry.name;
+    return liElem;
+  })
+  .forEach((liElem: HTMLLIElement) => entriesElem.appendChild(liElem));
+}
