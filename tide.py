1afe13005a6a9a31493b07760995005c48212f19
Kazuhiro Yamato <kz0817@gmail.com>
2025-02-27 19:28:22 +0900

    Add an API: filelist

--- tide.py
+++ tide.py
@@ -1,8 +1,15 @@
 #!/usr/bin/env python3
 import argparse
+import json
+import os
+import re
 from http.server import BaseHTTPRequestHandler, HTTPServer
+from pathlib import Path
+from urllib.parse import urlparse, parse_qs
 
 class TideHandler(BaseHTTPRequestHandler):
+
+    args = None
 
     def _dispatch(self, handler_map, path, default_handler):
         for re_path, handler in handler_map:
@@ -22,15 +29,39 @@ class TideHandler(BaseHTTPRequestHandler):
         with open('ui/main.html', 'rb') as file:
             self._response(file.read())
 
+    def _get_api_filelist(self, path):
+        parsed_path = urlparse(path)
+        query_params = parse_qs(parsed_path.query)
+        location = query_params.get('location', '/')[0]
+        # TODO: validate param_dir if it contains either '..'
+        local_dir = os.path.join(self.args.root_dir, location.lstrip('/'))
+
+        entries = []
+        body = {'location': location, 'entries': entries}
+
+        local_path = Path(local_dir)
+        items = local_path.iterdir()
+        for item in items:
+            entries.append({
+                'name': item.name,
+                'isDir': item.is_dir(),
+                'size':  item.stat().st_size,
+                'modifiedTime': item.stat().st_mtime,
+            })
+
+        self._response(json.dumps(body))
+
     def do_GET(self):
         self._dispatch(self._handlers_get, self.path, self._get_top_page)
 
     _handlers_get = [
+        (re.compile(r'/api/filelist'), _get_api_filelist)
     ]
 
 
 def run(args):
     server_address = ('', args.port)
+    TideHandler.args = args
     httpd = HTTPServer(server_address, TideHandler)
     print(f'Starting httpd server on port {args.port}...')
     httpd.serve_forever()
@@ -40,6 +71,7 @@ def main():
     parser = argparse.ArgumentParser(description="This is a sample script.")
 
     parser.add_argument('-p', '--port', default=3423)
+    parser.add_argument('-r', '--root-dir', default='')
 
     args = parser.parse_args()
     run(args)
