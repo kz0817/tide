aaf81f458759d2527f8cf6ff280a63a1f912bb91
Kazuhiro Yamato <kz0817@gmail.com>
2025-03-01 17:30:39 +0900

    Add a handler when directory link is clicked

--- ui-src/main.ts
+++ ui-src/main.ts
@@ -14,6 +14,26 @@ interface FileList {
   entries: DirEntry[];
 }
 
+const createEntryElement = (entry: DirEntry, currDir: string): HTMLElement => {
+  const elem: HTMLAnchorElement = document.createElement('a');
+  elem.textContent = entry.name;
+  elem.href = entry.isDir
+              ? '#'
+              : `/file/${currDir}/${entry.name}`
+  if (entry.isDir) {
+    elem.addEventListener('click', (event: MouseEvent) => {
+      showFileList(`${currDir}/${entry.name}`);
+    });
+  }
+  return elem;
+}
+
+const removeAllChildren = (elem: HTMLElement): void => {
+  while (elem.firstChild) {
+    elem.removeChild(elem.firstChild);
+  }
+}
+
 const showFileList = async (dir: string): Promise<void> => {
   const url = `/api/filelist?location=${dir}`;
   const response = await fetch(url);
@@ -27,11 +47,12 @@ const showFileList = async (dir: string): Promise<void> => {
   document.getElementById('location')!.textContent = fileList.location;
 
   const entriesElem: HTMLUListElement = document.getElementById('entries') as HTMLUListElement;
+  removeAllChildren(entriesElem);
 
   fileList.entries
   .map((entry: DirEntry) => {
     const liElem = document.createElement('li');
-    liElem.textContent = entry.name;
+    liElem.appendChild(createEntryElement(entry, dir));
     return liElem;
   })
   .forEach((liElem: HTMLLIElement) => entriesElem.appendChild(liElem));
